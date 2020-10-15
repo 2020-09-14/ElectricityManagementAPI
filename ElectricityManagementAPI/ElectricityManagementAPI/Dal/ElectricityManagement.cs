460a34a6f6b05f4a8c320dbb5a174ace46b266e3
ttf0512 <1799441665@qq.com>
2020-10-15 10:01:02 +0800

    第四次提交

---
+++ ElectricityManagementAPI/ElectricityManagementAPI/Dal/ElectricityManagement.cs
@@ -0,0 +1,15 @@
+﻿using Dapper;
+using ElectricityManagementAPI.Helper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectricityManagementAPI.Dal
+{
+    public class ElectricityManagement : IElectricityManagement
+    {
+      
+    }
+}
